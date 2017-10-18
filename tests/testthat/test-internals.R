a7320361c12403ae4246b702638d89892849c90f
wilsontom <wilsontom89@gmail.com>
2017-10-18 12:48:23 +0100

    update tests to line up with reformat

--- tests/testthat/test-internals.R
+++ tests/testthat/test-internals.R
@@ -4,14 +4,13 @@ test_that("internals", {
 
   load(system.file("extdata/chrom_a.rda", package = "pickeR"))
 
-  expect_true(is.data.frame(pickeR:::peak_detection(chrom_a[,"rt"], chrom_a[,"int"])))
 
   expect_true(is.numeric(pickeR:::estimate_noise(chrom_a[,"int"])))
 
   expect_true(is.vector(pickeR:::local_max(chrom_a[,"int"])))
   expect_true(is.vector(pickeR:::local_min(chrom_a[,"int"])))
 
-  peak_info <- pickeR:::peak_detection(chrom_a[,"rt"], chrom_a[,"int"])
+  peak_info <- find_peaks(chrom_a[,"rt"], chrom_a[,"int"])
   peak_merged <- pickeR:::merge_peaks(chrom_a[,"rt"], chrom_a[,"int"], idx = c(1:5), peak_info)
 
   expect_true(is.data.frame(peak_merged))
--- tests/testthat/test-plotting.R
+++ tests/testthat/test-plotting.R
@@ -4,10 +4,9 @@ test_that("plots", {
 
   load(system.file("extdata/chrom_a.rda", package = "pickeR"))
 
-  expect_true(is.null(chrom_plot(chrom_a[,"rt"], chrom_a[,"int"])))
   expect_error(chrom_plot(chrom_a[,"rt"], chrom_a[,"int"], c(1:nrow(chrom_a))))
-
-  peak_info <- pickeR:::peak_detection(chrom_a[,"rt"], chrom_a[,"int"])
+  expect_true(is.data.frame(find_peaks(chrom_a[,"rt"], chrom_a[,"int"])))
+  peak_info <- find_peaks(chrom_a[,"rt"], chrom_a[,"int"])
 
   expect_false(is.null(chrom_plot(chrom_a[,"rt"], chrom_a[,"int"], peak_info = peak_info)))
   expect_true(is.list(chrom_plot(chrom_a[,"rt"], chrom_a[,"int"], peak_info = peak_info)))
