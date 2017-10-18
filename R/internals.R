2073bd7c598be55dbc117a114c3743a372fea218
wilsontom <wilsontom89@gmail.com>
2017-10-18 12:47:31 +0100

    move some worker functions to internals; no use outside of pick_peaks

--- R/internals.R
+++ R/internals.R
@@ -83,6 +83,92 @@ inegrate_peak <- function(rt,int,range)
 
 
 
+#' Merge peaks
+#'
+#' Merge multipe detected peaks toegther into a single chromatographic feature
+#'
+#' @param rt a numeric vector of retention time
+#' @param int a numeric vector of intensity#' @param idx
+#' @param idx a numeric vector of peak id's to merge.
+#' @param peak_info a \code{data.frame} of peak info (see \code{\link{find_peaks}})
+#' @return an updated \code{peak_info} \code{data.frame}
+#'
+#' @keywords internal
+#' @author Tom Wilson \email{tpw2@@aber.ac.uk}
+
+merge_peaks <- function(rt,int, idx, peak_info)
+{
+
+  # find which is the apex max of idx
+
+  midx <- match(idx, peak_info[,"peak_id"])
+
+  apex_peak <- midx[which(peak_info[midx,"int"] == max(peak_info[midx,"int"]))]
+
+  merge_peak <-  midx[-which(peak_info[midx,"int"] == max(peak_info[midx,"int"]))]
+
+  peak_info[apex_peak, "rt_left"] <- min(peak_info[midx, "rt_left"])
+  peak_info[apex_peak, "left"] <- min(peak_info[midx, "left"])
+
+  peak_info[apex_peak, "rt_right"] <- max(peak_info[midx, "rt_right"])
+  peak_info[apex_peak, "right"] <- max(peak_info[midx, "right"])
+
+  peak_info <- peak_info[-merge_peak,]
+
+  apex_peak_ind <- match(apex_peak, peak_info[,"peak_id"])
+  peak_info[apex_peak_ind,"area"] <- inegrate_peak(rt,int,c(peak_info[apex_peak_ind,"left"]:peak_info[apex_peak_ind, "right"]))
+
+  return(peak_info)
+}
+
+
+#' Peak info
+#'
+#' Create a \code{peak_info} \code{data.frame}
+#'
+#' @param rt a numeric vector of retention time
+#' @param int a numeric vector of intensity#' @param idx
+#' @param peak_limits a \code{data.frame} indicating the left, right and apex indices for all detected peaks
+#' @return a \code{peak_info} \code{data.frame}
+#'
+#' @keywords internal
+#' @author Tom Wilson \email{tpw2@@aber.ac.uk}
+
+get_peak_info <- function(rt,int, peak_limits)
+{
+
+  # checks for rt int len and peak_limits return
+
+  peak_info <- data.frame(matrix(nrow = nrow(peak_limits), ncol = 7))
+  names(peak_info) <- c("peak_id", "rt", "rt_left", "rt_right", "int", "area", "peak_width")
+
+  for(i in seq_along(1:nrow(peak_limits))){
+    peak_info[i, "peak_id"] <- i
+    peak_info[i,"rt"] <- rt[peak_limits[i,"apex"]]
+    peak_info[i,"int"] <- int[peak_limits[i,"apex"]]
+    peak_info[i,"peak_width"] <- (rt[peak_limits[i,"right"]] * 60) - (rt[peak_limits[i,"left"]] * 60)
+
+    peak_info[i,"rt_left"] <- rt[peak_limits[i,"left"]]
+    peak_info[i,"rt_right"] <- rt[peak_limits[i,"right"]]
+
+    peak_info[i, "area"] <- inegrate_peak(rt,int, c(peak_limits[i,"left"]:peak_limits[i,"right"]))
+  }
+
+  peak_info[,"rt"] <- round(peak_info[,"rt"], digits = 2)
+  peak_info[,"rt_left"] <- round(peak_info[,"rt_left"], digits = 2)
+  peak_info[,"rt_right"] <- round(peak_info[,"rt_right"], digits = 2)
+  peak_info[,"peak_width"] <- round(peak_info[,"peak_width"], digits = 1)
+
+  peak_info <- data.frame(peak_info, left = peak_limits[,"left"], apex = peak_limits[,"apex"],
+                          right = peak_limits[,"right"])
+  return(peak_info)
+
+}
+
+
+
+
+
 
 
 
