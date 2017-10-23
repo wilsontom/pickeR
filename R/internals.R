896c3fe8e3df5b06afe521a94a0de3f8e9f3fefa
wilsontom <wilsontom89@gmail.com>
2017-10-23 14:15:59 +0100

    restructure and fix peak smoothing

--- R/internals.R
+++ R/internals.R
@@ -42,8 +42,6 @@ local_max <- function(x)
 }
 
 
-
-
 #' Estimate Noise
 #'
 #' Make an approximate estimation of baseline noise by taking the mean intensity of all local minima's after removing regions
@@ -55,20 +53,42 @@ local_max <- function(x)
 #' @keywords internal
 
 estimate_noise <- function(x)
-  {
+{
 
   lmax <- local_max(x)
   lmin <- local_min(x[-lmax])
 
   #return(as.numeric(median(x[lmin])))
   return(as.numeric(mean(x[lmin])))
+}
+
+
+
+#' Smooth Peak
+#' @keywords internal
+smooth_peak <- function(rt,int, ford)
+{
+
+  f <- seq(from = 3, to = 51, by = 2)
+
+  filtered_int <- NULL
+  for(i in seq_along(f)){
+    filtered_int[[i]] <- pracma::savgol(int, f[i], forder = ford)
   }
 
+  noise <- estimate_noise(int)
 
-#' Integrate Peak
-#'
-#'
-#' @keywords internal
+  min_int <- which(unlist(purrr:::map(filtered_int, ~{abs(min(.)) > noise})) == FALSE)
+
+  selected_f <- f[max(min_int)]
+
+  smoothed_int <- pracma::savgol(int, fl = selected_f, forder = ford, dorder = 0)
+
+  smoothed_int[smoothed_int < 0] <- 0
+
+    return(smoothed_int)
+
+}
 
 
 inegrate_peak <- function(rt,int,range)
@@ -80,8 +100,6 @@ inegrate_peak <- function(rt,int,range)
 
   return(as.numeric(peak_area))
 }
-
-
 
 #' Merge peaks
 #'
@@ -120,53 +138,6 @@ merge_peaks <- function(rt,int, idx, peak_info)
 
   return(peak_info)
 }
-
-
-#' Peak info
-#'
-#' Create a \code{peak_info} \code{data.frame}
-#'
-#' @param rt a numeric vector of retention time
-#' @param int a numeric vector of intensity#' @param idx
-#' @param peak_limits a \code{data.frame} indicating the left, right and apex indices for all detected peaks
-#' @return a \code{peak_info} \code{data.frame}
-#'
-#' @keywords internal
-#' @author Tom Wilson \email{tpw2@@aber.ac.uk}
-
-get_peak_info <- function(rt,int, peak_limits)
-{
-
-  # checks for rt int len and peak_limits return
-
-  peak_info <- data.frame(matrix(nrow = nrow(peak_limits), ncol = 7))
-  names(peak_info) <- c("peak_id", "rt", "rt_left", "rt_right", "int", "area", "peak_width")
-
-  for(i in seq_along(1:nrow(peak_limits))){
-    peak_info[i, "peak_id"] <- i
-    peak_info[i,"rt"] <- rt[peak_limits[i,"apex"]]
-    peak_info[i,"int"] <- int[peak_limits[i,"apex"]]
-    peak_info[i,"peak_width"] <- (rt[peak_limits[i,"right"]] * 60) - (rt[peak_limits[i,"left"]] * 60)
-
-    peak_info[i,"rt_left"] <- rt[peak_limits[i,"left"]]
-    peak_info[i,"rt_right"] <- rt[peak_limits[i,"right"]]
-
-    peak_info[i, "area"] <- inegrate_peak(rt,int, c(peak_limits[i,"left"]:peak_limits[i,"right"]))
-  }
-
-  peak_info[,"rt"] <- round(peak_info[,"rt"], digits = 2)
-  peak_info[,"rt_left"] <- round(peak_info[,"rt_left"], digits = 2)
-  peak_info[,"rt_right"] <- round(peak_info[,"rt_right"], digits = 2)
-  peak_info[,"peak_width"] <- round(peak_info[,"peak_width"], digits = 1)
-
-  peak_info <- data.frame(peak_info, left = peak_limits[,"left"], apex = peak_limits[,"apex"],
-                          right = peak_limits[,"right"])
-  return(peak_info)
-
-}
-
-
-
 
 
 
